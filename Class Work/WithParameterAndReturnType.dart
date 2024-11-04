2c97fe1ec0686e53867533ee9f8649608706974b
vinnietito <vincentnjuguna69@gmail.com>
2024-11-04 19:30:01 +0300

    Commits

---
+++ Class Work/WithParameterAndReturnType.dart
@@ -0,0 +1,12 @@
+//Function to add two integers and return the result
+int add(int a, int b){
+  return a + b;
+}
+
+void main(){
+  //Calling the function with two integers
+  int result = add(10, 20);
+
+  //Printing the result
+  print("The sum of 10 and 20 is : $result");
+}
