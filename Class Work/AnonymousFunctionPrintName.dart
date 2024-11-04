2d6fa253565020d62ced251ad305557f4fbfc02c
vinnietito <vincentnjuguna69@gmail.com>
2024-11-04 19:36:42 +0300

    Commits

---
+++ Class Work/AnonymousFunctionPrintName.dart
@@ -0,0 +1,10 @@
+void main(){
+  //Anonymous function assigned to the variable 'printName'
+  var printName = () {
+    //Print message inside the anonymous function
+    print("My name is Vincent");
+  };
+
+  //Call the anonymous function
+  printName();
+}
