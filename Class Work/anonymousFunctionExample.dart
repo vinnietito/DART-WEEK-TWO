50bfccc05aa4d289353edf3e3474d8442910aaf3
vinnietito <vincentnjuguna69@gmail.com>
2024-11-04 20:16:30 +0300

    Commits

---
+++ Class Work/anonymousFunctionExample.dart
@@ -0,0 +1,9 @@
+void main(){
+  //List of Items
+  var items = ['Laptop', 'Tablet', 'Smartphone', 'SmartWatch'];
+
+  //Using an anonymous function with forEach to print each item
+  items.forEach((item) {
+    print(item);
+  });
+}
