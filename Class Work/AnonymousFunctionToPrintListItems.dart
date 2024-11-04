bc127c700d4b793b3dc7e67c45b57928e1c7df88
vinnietito <vincentnjuguna69@gmail.com>
2024-11-04 20:05:34 +0300

    Commits

---
+++ Class Work/AnonymousFunctionToPrintListItems.dart
@@ -0,0 +1,9 @@
+void main() {
+  // List of fruits
+  var fruits = ['Apple', 'Banana', 'Cherry', 'Date'];
+
+  // Using an anonymous function with forEach to print each fruit
+  fruits.forEach((fruit) {
+    print(fruit);
+  });
+}
---
+++ Class Work/AnonymousFunctionsExample.dart
@@ -0,0 +1,8 @@
+void main() {
+  var machines = ["Benz", "Volvo", "Toyota", "Range Rover", "Porsche", "Bentley"];
+
+  // Using an anonymous function with forEach to print each machine
+  machines.forEach((machines) {
+    print(machines);
+  });
+}
