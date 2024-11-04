7c4ca9e2ada162fe2ecae34671e015de520a0d9a
vinnietito <vincentnjuguna69@gmail.com>
2024-11-04 19:20:02 +0300

    Commits

---
+++ Class Work/NoParameterAndReturnTtpe.dart
@@ -0,0 +1,12 @@
+//Function to return Instructor's name
+String Instructorname(){
+  return "Allan";
+}
+
+void main(){
+  //Calling the function and storing the result
+  String instructor = Instructorname();
+
+  //Printing the instructor's name
+  print("The Instructor's name is: $instructor");
+}
---
+++ Class Work/NoParameterNoReturnType.dart
@@ -0,0 +1,9 @@
+//No parameter and no return type
+void main(){
+  printName();
+}
+
+//no parameter and no return type(nothing in parameter and no return type)
+void printName(){
+  print("Vincent Kimanthi is a student");
+}
